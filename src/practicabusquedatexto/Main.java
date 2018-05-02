2159cbfbc864cab0c7c727380e40915667ef38a7
angelloro <Angel.Loro@alu.uclm.es>
2018-05-02 20:22:11 +0200

    version 1 menu

--- src/practicabusquedatexto/Main.java
+++ src/practicabusquedatexto/Main.java
@@ -1,23 +1,62 @@
 package practicabusquedatexto;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import utilidades.leer;
 
 /**
  * @author Angel
- **/
-
+ *
+ */
 public class Main {
-    
-    public static void main(String[] args) throws IOException {
-        String archivo="quijote1.txt";
-        lecturaFicheros f=new lecturaFicheros();
-        String texto= f.crearTexto(archivo);
-        leer.pln(texto);
+
+    public static void main(String[] args) throws IOException{
+        busqueda();
+    }
+    public static String creacionTexto() throws IOException{
+        String archivo = "quijote1.txt";
+        lecturaFicheros f = new lecturaFicheros();
+        String texto = f.crearTexto(archivo);
+        return texto;
+    }
+    public static void busqueda() throws IOException{
+        String texto= creacionTexto();
+        String patron = leer.cadena("Introduzca el patron a buscar:\n");
+        Boolean seguir=true;
         PracticaBusquedaTexto p = new PracticaBusquedaTexto();
-        leer.pln("Shift Or: " + p.ShiftOr("el", texto).size());
-        leer.pln("Karp Rabin: " + p.KarpRabin("el", texto).size());
-        leer.pln("Knuth Morris Prat: " + p.KnuthMorrisPrat("el", texto).size());
-       
+        ArrayList<Integer> ocurrencias = new ArrayList<Integer>();
+        do{
+        switch (leer.entero("Indica la opcion que desea realizar:\n\t1)Shift Or\n\t2)Karp Rabin\n\t3)Knuth Morris Prat\n\t4)Boyer Moore\n\t5)Cambiar patron\n\t6)Salir\n")) {
+            case 1:
+                ocurrencias=p.ShiftOr(patron, texto);
+                leer.pln("Numero de ocurrencias: " + ocurrencias.size());
+                leer.pln("Posiciones de las ocurrencias: "+ocurrencias);
+                break;
+            case 2:
+                ocurrencias=p.KarpRabin(patron, texto);
+                leer.pln("Numero de ocurrencias: " + ocurrencias.size());
+                leer.pln("Posiciones de las ocurrencias: "+ocurrencias);
+                break;
+            case 3:
+                ocurrencias=p.KnuthMorrisPrat(patron, texto);
+                leer.pln("Numero de ocurrencias: " + ocurrencias.size());
+                leer.pln("Posiciones de las ocurrencias: "+ocurrencias);
+                break;
+            case 4:
+                ocurrencias=p.BoyerMoore(patron, texto);
+                leer.pln("Numero de ocurrencias: " + ocurrencias.size());
+                leer.pln("Posiciones de las ocurrencias: "+ocurrencias);
+                break;
+            case 5:
+                patron = leer.cadena("Introduzca el patron a buscar:\n");
+                break;
+            case 6:
+                seguir=false;
+                break;
+            default:
+                leer.pln("Opcion invalida");
+                break;
+        }
+        }while(seguir);
     }
 }
